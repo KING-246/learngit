d448fdf00db4f8b3aec780cef882cdbee58b23a7
king-246 <mking-246@outlook.com>
2021-01-24 21:11:56 +0800

    add demo.sh

---
+++ buff1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+    printf("C语言中文网");
+    fflush(stdout);  //本次输出结束后立即清空缓冲区
+    sleep(5);
+    printf("http://c.biancheng.net\n");
+   
+    return 0;
+}
